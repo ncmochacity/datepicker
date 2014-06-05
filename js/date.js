691288270f5d624dc1333fa0d0103810149b3ffa
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-06-04 21:31:49 -0500

    first

---
+++ js/date.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#date").datepicker({
+		inline:true,
+		firstDay:1,
+		showOtherMonths:true,
+		dayNamesMin: ['Sun', 'Mon' , 'Tue' , 'Wed' , 'Thu' , 'Fri' , 'Sat']
+	})
+	$("#arrive").datepicker({
+		inline:true,
+		firstDay:1,
+		showOtherMonths:true,
+		dayNamesMin: ['Sun', 'Mon' , 'Tue' , 'Wed' , 'Thu' , 'Fri' , 'Sat']
+	})
+})

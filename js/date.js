a7b86a18a24e4a38b1be2d76e05bf2a7fb32a25c
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-06-05 21:41:48 -0500

    final

--- js/date.js
+++ js/date.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-	$("#date").datepicker({
+	$("#date, #arrive").datepicker({
 		inline:true,
 		firstDay:1,
 		showOtherMonths:true,
 		dayNamesMin: ['Sun', 'Mon' , 'Tue' , 'Wed' , 'Thu' , 'Fri' , 'Sat']
 	})
-	$("#arrive").datepicker({
-		inline:true,
-		firstDay:1,
-		showOtherMonths:true,
-		dayNamesMin: ['Sun', 'Mon' , 'Tue' , 'Wed' , 'Thu' , 'Fri' , 'Sat']
+	var current=$("dt");
+	$("dt").click(function(){
+		$("dt").removeClass("current").addClass("not_current");
+		//apply not_current class to all the description terms
+		//then when selected, apply current class to the selected dt while the unselected ones apply to the not_current class
+		$(this).addClass("current").removeClass("not_current");
+		});
+	var dd=$("dd");
+	dd.hide();
+	$("dt").on("click",function(){
+		$("dd").slideUp();	
+		$(this).parent().find("dd").show();
 	})
 })
